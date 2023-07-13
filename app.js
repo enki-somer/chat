ae4bfda8f78a4c0a7f73ed9a6783eee6a3dd8939
enki-somer <enkisomer@gmail.com>
2023-07-14 00:01:00 +0300

    Fri 07/14/2023,  0:00:59.71

---
+++ app.js
@@ -0,0 +1,25 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ["http://192.168.5.224:5500"],
+  },
+});
+const users = {};
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+  });
+  // socket.emit('chat-message' , 'hallo word')
+  socket.on("send-chat-message", (message) => {
+    console.log(message);
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
